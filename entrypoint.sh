c33974ff2720a4f4117da49849585a095e46d84c
kunikada <kunikada@gmail.com>
2022-04-19 09:00:27 +0000

    correspond to smtp auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 env > .env
 
-echo testpass | saslpasswd2 -c -p -u `postconf -h myhostname` test
+for account in `echo $AUTH_USERS | tr ',' ' '`; do
+    auth=(`echo $account | tr ':' ' '`)
+    echo ${auth[1]} | saslpasswd2 -c -p -u `postconf -h myhostname` ${auth[0]}
+done
 cp -p /etc/sasldb2 /var/spool/postfix/etc
 
 service saslauthd start
